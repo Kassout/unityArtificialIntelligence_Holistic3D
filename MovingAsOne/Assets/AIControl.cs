63c729cffb7e67b8063e9c5d0cf07e512794b16e
mheliot <maxime.heliotpro@gmail.com>
2021-04-05 15:26:48 +0200

    Add crowd control techniques

---
+++ MovingAsOne/Assets/AIControl.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour {
+
+	private GameObject[] goalLocations;
+	private NavMeshAgent agent;
+	private Animator anim;
+	private float speedMult;
+	private float detectionRadius = 20;
+	private float fleeRadius = 10;
+
+	void ResetAgent()
+	{
+		speedMult = Random.Range(0.1f, 1.5f);
+		agent.speed = 2 * speedMult;
+		agent.angularSpeed = 120;
+		anim.SetFloat("speedMult", speedMult);
+		anim.SetTrigger("isWalking");
+		agent.ResetPath();
+	}
+
+	public void DetectNewObstacle(Vector3 position)
+	{
+		if (Vector3.Distance(position, transform.position) < detectionRadius)
+		{
+			Vector3 fleeDirection = (transform.position - position).normalized;
+			Vector3 newGoal = transform.position + fleeDirection * fleeRadius;
+
+			NavMeshPath path = new NavMeshPath();
+			agent.CalculatePath(newGoal, path);
+
+			if (path.status != NavMeshPathStatus.PathInvalid)
+			{
+				agent.SetDestination(path.corners[path.corners.Length - 1]);
+				anim.SetTrigger("isRunning");
+				agent.speed = 10;
+				agent.angularSpeed = 500;
+			}
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		goalLocations = GameObject.FindGameObjectsWithTag("Goal");
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(goalLocations[Random.Range(0,goalLocations.Length)].transform.position);
+		
+		anim = GetComponent<Animator>();
+		anim.SetFloat("wOffset", Random.Range(0,1));
+		ResetAgent();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (agent.remainingDistance < 1)
+		{
+			ResetAgent();
+			agent.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+		}
+	}
+}
---
+++ MovingAsOne/Assets/DropCylinder.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class DropCylinder : MonoBehaviour
+{
+    public GameObject obstacle;
+    private GameObject[] agents;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("Agent");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hitInfo;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray.origin, ray.direction, out hitInfo))
+            {
+                Instantiate(obstacle, hitInfo.point, obstacle.transform.rotation);
+                foreach (var a in agents)
+                {
+                    a.GetComponent<AIControl>().DetectNewObstacle(hitInfo.point);
+                }
+            }
+        }        
+    }
+}
