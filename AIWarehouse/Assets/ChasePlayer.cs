ef81b55a0f7ce782b87e3f11d9870ba9871821b4
mheliot <maxime.heliotpro@gmail.com>
2021-04-07 01:30:49 +0200

    Add AIWarehouse Challenge

---
+++ AIWarehouse/Assets/ChasePlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class ChasePlayer : MonoBehaviour
+{
+    private GameObject player;
+    private NavMeshAgent agent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(player.transform.position);
+    }
+}
