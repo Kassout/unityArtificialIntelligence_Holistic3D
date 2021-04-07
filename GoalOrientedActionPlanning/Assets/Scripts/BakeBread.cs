459179699ae2cda6f81eb3b4d84362a58cdf0831
mheliot <maxime.heliotpro@gmail.com>
2021-04-07 18:48:50 +0200

    Add GoalOrientedActionPlanning techniques

---
+++ GoalOrientedActionPlanning/Assets/Scripts/BakeBread.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+public class BakeBread : GoapAction
+{
+    private bool completed = false;
+    private float startTime = 0;
+
+    public float workDuration = 2; // seconds
+
+    public BakeBread()
+    {
+        addPrecondition("hasFlour", true);
+        addEffect("doJob", true);
+        name = "BakeBread";
+    }
+
+    public override void reset()
+    {
+        completed = false;
+        startTime = 0;
+    }
+
+    public override bool isDone()
+    {
+        return completed;
+    }
+
+    public override bool requiresInRange()
+    {
+        return true;
+    }
+
+    public override bool checkProceduralPrecondition(GameObject agent)
+    {
+        return true;
+    }
+
+    public override bool perform(GameObject agent)
+    {
+
+        if (startTime == 0)
+        {
+            Debug.Log("Starting: " + name);
+            startTime = Time.time;
+        }
+
+        if (Time.time - startTime > workDuration)
+        {
+            Debug.Log("Finished: " + name);
+            GetComponent<Inventory>().flourLevel -= 2;
+            GetComponent<Inventory>().breadLevel += 1;
+            completed = true;
+        }
+
+        return true;
+    }
+}
---
+++ GoalOrientedActionPlanning/Assets/Scripts/DeliverWheat.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliverWheat : GoapAction
+{
+    private bool completed = false;
+    private float startTime = 0;
+
+    public float workDuration = 2; // seconds
+    public Inventory inv;
+
+    public DeliverWheat()
+    {
+        addPrecondition("hasWheat", true);
+        addEffect("doJob", true);
+        name = "DeliverWheat";
+    }
+
+    public override void reset()
+    {
+        completed = false;
+        startTime = 0;
+    }
+
+    public override bool isDone()
+    {
+        return completed;
+    }
+
+    public override bool requiresInRange()
+    {
+        return true;
+    }
+
+    public override bool checkProceduralPrecondition(GameObject agent)
+    {
+        return true;
+    }
+
+    public override bool perform(GameObject agent)
+    {
+
+        if (startTime == 0)
+        {
+            Debug.Log("Starting: " + name);
+            startTime = Time.time;
+        }
+
+        if (Time.time - startTime > workDuration)
+        {
+            Debug.Log("Finished: " + name);
+            inv.flourLevel++;
+            completed = true;
+        }
+
+        return true;
+    }
+}
