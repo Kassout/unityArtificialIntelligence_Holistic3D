62691802ecbc87f5f3de1d8c37e08f15be792005
mheliot <maxime.heliotpro@gmail.com>
2021-04-07 02:08:05 +0200

    Add Fish Flock Speed Challenge

---
+++ FishFlockSpeedChallenge/Assets/FlockManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using Quaternion = UnityEngine.Quaternion;
+using Vector3 = UnityEngine.Vector3;
+
+public class FlockManager : MonoBehaviour
+{
+    public GameObject fishPrefab;
+    public int numFish = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+    public Vector3 goalPos;
+
+    [Header("Fish Settings")] 
+    [Range(0.0f, 5.0f)] public float minSpeed;
+    [Range(0.0f, 5.0f)] public float maxSpeed;
+    [Range(1.0f, 10.0f)] public float neighbourDistance;
+    [Range(0.0f, 5.0f)] public float rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        allFish = new GameObject[numFish];
+
+        for (int i = 0; i < numFish; i++)
+        {
+            Vector3 pos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.y, swimLimits.y), Random.Range(-swimLimits.z, swimLimits.z));
+            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+            allFish[i].GetComponent<Flock>().myManager = this;
+        }
+
+        goalPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Random.Range(0, 100) < 10)
+        {
+            goalPos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x),
+                Random.Range(-swimLimits.y, swimLimits.y), Random.Range(-swimLimits.z, swimLimits.z));
+        }
+    }
+}
